657db16dd23893dc7a640b22bb8115f10700c896
hamzaelmouka <hmzelmouka@gmail.com>
2026-09-16 17:53:43 +0200

    ch1

---
+++ challenge1.js
@@ -0,0 +1,4 @@
+const prompt = require('prompt-sync')();
+const name = prompt('What is your name? ');
+const age = prompt('What is your age? ');
+console.log(`Hello ${name}, you are ${age} years old.`);
